c10f4cde4848b1f7d5037d3f446cba6a506d5b7a
darsay8 <darsay.one@gmail.com>
2022-04-02 21:41:24 -0300

    login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ const createUser = async (req, res = response) => {
     if (user) {
       return res.status(400).json({
         ok: false,
-        msg: 'E-mail already in use',
+        msg: 'Email already in use',
       });
     }
 
@@ -36,15 +36,40 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.status(201).json({
-    post: true,
-    action: 'Login User',
-    email,
-    password,
-  });
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Email does not exist',
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password is not valid',
+      });
+    }
+
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      message: 'Error',
+    });
+  }
 };
 
 const validateToken = (req, res = response) => {
