93d61c42b129cb8b5b4e852422a5bf0ae2984d5f
darsay8 <darsay.one@gmail.com>
2022-04-04 17:21:12 -0400

    get events

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,11 @@
 const { response } = require('express');
 const Event = require('../models/Event');
 
-const getEvents = (req, res = response) => {
-  return res.status(200).json({
+const getEvents = async (req, res = response) => {
+  const events = await Event.find().populate('user', 'name');
+  res.json({
     ok: true,
-    msg: 'get events',
+    events,
   });
 };
 
